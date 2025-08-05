ff919cfd88fc92b622c89da7738d184ac55e2c9d
Dhruv Divecha <157246271+dhruvdivecha@users.noreply.github.com>
2025-08-05 16:37:12 +0300

    First commit

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,15 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(() => ({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       '/api': 'http://localhost:8000', 
     },
   },
-})
+  preview: {
+    host: '0.0.0.0',
+    allowedHosts: ['selcom-bot-frontend.onrender.com']
+  }
+}))
